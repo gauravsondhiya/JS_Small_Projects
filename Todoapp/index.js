6bd930bdce15b37f1bc1bd38f4bc3bcc87b894e0
gauravsondhiya <gauravsondhiya1503@gmail.com>
2025-03-16 15:59:15 +0530

    minor

--- Todoapp/index.js
+++ Todoapp/index.js
@@ -2,23 +2,26 @@ document.getElementById("btn").addEventListener("click", btnclick);
 
 let data = JSON.parse(localStorage.getItem("data")) || [];
 function btnclick() {
- 
   let inputvalue = document.getElementById("inputbox").value;
   if(inputvalue!=""){
     data.push(inputvalue);
     localStorage.setItem("data", JSON.stringify(data));
     
     showoutput()
+    document.getElementById("gaur").innerText=" "
     document.getElementById("inputbox").value = ""
   }else{
    document.getElementById("gaur").innerText="Enter Todo"
-
   }
   
 }
 function showoutput(){
   document.getElementById("values").innerText=""
-  data.map((values)=>{
+  data.map((values,i)=>{
+    let h2= document.createElement("h1")
+    h2.innerText = i+1
+
+
     let big1 = document.createElement("div")
     big1.setAttribute("id","big1")
   
@@ -30,7 +33,7 @@ function showoutput(){
     })
     btndelete.innerText= "Done"
     btndelete.setAttribute("id","deletebtn")
-    big1.append(h1,btndelete)
+    big1.append(h2,h1,btndelete)
     
   
     document.getElementById("values").append(big1)
